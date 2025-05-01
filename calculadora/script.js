7e051eefcba01f97edccf14f1001ee68dea2003d
wel-lopes <weberth.001@gmail.com>
2025-05-01 15:59:40 -0300

    refactor: internal improvement

--- calculadora/script.js
+++ calculadora/script.js
@@ -8,11 +8,35 @@ function removerUltimo(){
     let display = document.getElementById('display');
     display.value = display.value.slice(0, -1);
 }
-function calcular(){
+function calcular() {
     try {
-        let resultado = eval(document.getElementById('display').value);
-    document.getElementById('display').value = resultado;
-} catch{
-    document.getElementById('display').value = 'Erro';
+        let expressao = document.getElementById('display').value;
+        let resultado = Function(`return ${expressao}`)();
+        document.getElementById('display').value = resultado;
+    } catch {
+        document.getElementById('display').value = 'Erro';
     }
 }
+
+document.addEventListener('keydown', function (e) {
+    const tecla = e.key;
+    const permitido = "0123456789+-*/.";
+
+    // Conecta tecla física com botão visual:
+    const botao =document.querySelector(`button[data-tecla="${tecla}"]`);
+
+    if (botao) {
+        botao.classList.add("ativo");
+    setTimeout(() => botao.classList.remove("ativo"), 150)
+    }
+    //Funções da calculadora:
+    if (!isNaN(tecla) || permitido.includes(tecla)) {
+        adicionar(tecla);
+    } else if (tecla === 'Enter') {
+        calcular();
+    } else if (tecla === 'Backspace') {
+        removerUltimo();
+    } else if (tecla === 'Escape') {
+        limpar();
+    }
+});
